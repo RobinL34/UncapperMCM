4e63d60f4a1298c90992b79203a83123a753357e
Robin <robinlooup@gmail.com>
2026-09-03 10:34:10 +0200

    refactor: extract Papyrus enchanting bindings

--- src/Papyrus/Bindings.h
+++ src/Papyrus/Bindings.h
@@ -24,3 +24,24 @@ std::int32_t PapyrusGetSkillCap(
 std::int32_t PapyrusGetFormulaCap(
     RE::StaticFunctionTag *,
     std::int32_t skillSlot);
+
+bool PapyrusSetEnchantMagnitudeCap(
+    RE::StaticFunctionTag *,
+    std::int32_t value);
+
+bool PapyrusSetEnchantChargeCap(
+    RE::StaticFunctionTag *,
+    std::int32_t value);
+
+void PapyrusSetEnchantUseLinearCharge(
+    RE::StaticFunctionTag *,
+    bool enabled);
+
+std::int32_t PapyrusGetEnchantMagnitudeCap(
+    RE::StaticFunctionTag *);
+
+std::int32_t PapyrusGetEnchantChargeCap(
+    RE::StaticFunctionTag *);
+
+bool PapyrusGetEnchantUseLinearCharge(
+    RE::StaticFunctionTag *);
